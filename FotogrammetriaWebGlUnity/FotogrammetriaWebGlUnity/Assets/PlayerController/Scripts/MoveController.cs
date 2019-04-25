a22711d897dd2617c209215ff368d65e47ce0685
Wilk <piotr.blonski@coderscrew.pl>
2019-04-25 19:14:57 +0200

    Controller

---
+++ FotogrammetriaWebGlUnity/FotogrammetriaWebGlUnity/Assets/PlayerController/Scripts/MoveController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveController : MonoBehaviour
+{
+    [SerializeField] private string horizontalInputName;
+    [SerializeField] private string verticalInputName;
+    [SerializeField] private float movementSpeed;
+
+    private CharacterController charController;
+
+    private void Awake()
+    {
+        charController = GetComponent<CharacterController>();
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerMovement();
+    }
+    private void PlayerMovement()
+    {
+        float vertInput = Input.GetAxis(verticalInputName) * movementSpeed * Time.deltaTime;
+        float horizInput = Input.GetAxis(horizontalInputName) * movementSpeed * Time.deltaTime;
+
+        Vector3 forwardMovement = transform.forward * vertInput;
+        Vector3 rightMovement = transform.right * horizInput;
+
+        charController.SimpleMove(forwardMovement + rightMovement);
+
+    }
+}
