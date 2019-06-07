80fca8145a7c336119abdb6eb56ef130716013dc
Wilk <piotr.blonski@coderscrew.pl>
2019-06-07 19:10:44 +0200

    Interactive

---
+++ FotogrammetriaWebGlUnity/FotogrammetriaWebGlUnity/Assets/scripts/Abstract Classes/Interactive.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+public abstract class Interactive : MonoBehaviour
+{
+    public float InteractionRange = 3f;
+    public bool active = true;
+    protected abstract void Interaction();
+    public void Interact()
+    {
+        if(active)
+        {
+            Interaction();
+        }
+    }
+    void OnDrawGizmos()
+    {
+        // Draw a yellow sphere at the transform's position
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, InteractionRange);
+    }
+  
+}
---
+++ FotogrammetriaWebGlUnity/FotogrammetriaWebGlUnity/Assets/scripts/ExampleInteractiveObject.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExampleInteractiveObject : Interactive
+{
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    protected override void Interaction()
+    {
+        Debug.Log("Object Interacted");
+        Renderer rend = GetComponent<Renderer>();
+        if (rend.material.color == Color.red)
+        {
+            rend.material.SetColor("_Color", Color.white);
+        }else
+        rend.material.SetColor("_Color", Color.red);
+    }
+
+}
